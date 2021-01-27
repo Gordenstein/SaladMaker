dcd7ba28896c2a917dd1651ddb3a371eedb494c1
Eugene Gordenstein <e.gordenstein@gmail.com>
2021-01-27 13:16:43 +0200

    Initial Commit

---
+++ SaladMaker/SaladMakerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SaladMakerApp.swift
+//  SaladMaker
+//
+//  Created by Eugene Gordenstein on 1/27/21.
+//
+
+import SwiftUI
+
+@main
+struct SaladMakerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
